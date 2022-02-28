91a50958da8d19c51566f1f2b8d3c0ecbd8bc558
Petar Kobakov <pkobakov@gmail.com>
2022-02-28 22:56:45 +0200

    First Steps in TypeScript

--- Intro to Angular and TypeScript/Demos/Test/test.ts
+++ Intro to Angular and TypeScript/Demos/Test/test.ts
@@ -1,7 +1,17 @@
-let isOpen:boolean;
+let isOpen: boolean;
 
 
 isOpen = true;
 console.log(isOpen);
 
+class Person {
 
+    constructor(public name: string, public age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let ivan: Person;
+ivan = new Person('Ivan', 30);
+console.log(ivan.name);
