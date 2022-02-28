0f9ea022cefd1229ce2d6dfa8ffe4ade7dfecf3c
Petar Kobakov <pkobakov@gmail.com>
2022-03-01 00:22:46 +0200

    Inheritance in TypeScript

--- Intro to Angular and TypeScript/Demos/Test/test.ts
+++ Intro to Angular and TypeScript/Demos/Test/test.ts
@@ -4,14 +4,34 @@ let isOpen: boolean;
 isOpen = true;
 console.log(isOpen);
 
-class Person {
 
-    constructor(public name: string, public age: number) {
-        this.name = name;
-        this.age = age;
+
+enum UserRole {
+
+}
+
+interface IUserNewData {
+    newName: string;
+    newAge: number;
+}
+
+class Person {
+    constructor(
+        public name: string,
+        public age: number) 
+        { }
+
+    setData(newData: IUserNewData) {
+        this.age = newData.newAge;
+        this.name = newData.newName;
+    }  
+
+    getAge() {
+        this.age;
     }
 }
 
-let ivan: Person;
-ivan = new Person('Ivan', 30);
-console.log(ivan.name);
+const ivan: Person = new Person('Ivan', 30);
+const newData: IUserNewData ={ newAge: 30, newName: 'Pesho'};
+
+ivan.setData(newData);
