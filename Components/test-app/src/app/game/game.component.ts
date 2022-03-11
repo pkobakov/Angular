a190db7b8d2e8c80d7442f975837bf9bfbc612bb
Petar Kobakov <pkobakov@gmail.com>
2022-03-11 11:12:34 +0200

    Add to tsconfig.json "strictPropertyOInizialization" and modify event handler of the button

--- Components/test-app/src/app/game/game.component.ts
+++ Components/test-app/src/app/game/game.component.ts
@@ -11,14 +11,27 @@ interface Game {
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.css'],
 })
+
+
 export class GameComponent {
+    priceTextColor = 'red';
+    priceShouldBePurple: boolean;
+
     games: Game[] = [
         {title: 'Minecraft', price: 4.50, img: 'https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP'},
         {title: 'Candy Crash', price: 7.89, img: 'https://media.pocketgamer.biz/2014/8/20830/candy-crush-art-r225x.jpg'},
         {title:'Counter Strike', price: 12.33, img: 'https://www.mobygames.com/images/shots/l/83832-counter-strike-condition-zero-windows-screenshot-title-screen.jpg'},
     ]
 
-    handleExpandContentClick(): void{
-        alert('Expand Clicked');
+    handleExpandContentClick(gamesContainer: HTMLElement): void{
+        this.priceShouldBePurple = !this.priceShouldBePurple;
+
+        if (this.priceShouldBePurple) {
+            this.priceTextColor = 'blueviolet';
+        } else{
+            this.priceTextColor = 'green';
+        }
+
+        console.log(gamesContainer.children);
     }
 }
