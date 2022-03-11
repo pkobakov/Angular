c7aa57f3c192e1796ff979a651691a6cd6da4594
Petar Kobakov <pkobakov@gmail.com>
2022-03-11 15:29:24 +0200

    Add directives

--- Components/idakiev-demo-app/src/app/app.component.ts
+++ Components/idakiev-demo-app/src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+const users = [
+  {
+     firstname: 'Pesho',
+     age: 33
+  },
+
+  {
+     firstname: 'Gosho',
+     age: 47
+  },
+
+  {
+    firstname: 'Misho',
+    age: 55
+  }
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +25,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'idakiev-demo-app';
   welcomeMessage = 'Hello from my app';
+  isVisible = false;
+
+  users = users;
+
+  constructor(){
+
+  }
+
+  toggleHandler(): void {
+    this.isVisible = !this.isVisible;
+  }
 }
--- Components/idakiev-demo-app/src/app/name-input/name-input.component.ts
+++ Components/idakiev-demo-app/src/app/name-input/name-input.component.ts
@@ -11,9 +11,16 @@ export class NameInputComponent implements OnInit {
 
   constructor() { }
 
+
+  inputKeyupHandler(event: KeyboardEvent): void {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
   btnClickHandler(value: any): void {
     console.log('btn has clicked', value);
   }
+
+ 
 
   ngOnInit(): void {
   }
