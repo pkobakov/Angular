9c413940d561e3561373379cc465aa6b3d4ea031
Petar Kobakov <pkobakov@gmail.com>
2022-03-12 16:46:43 +0200

    Create Article Data

---
+++ Components/Lab/articles-app/src/app/data/data.ts
@@ -0,0 +1,17 @@
+import { data } from './seed';
+import { Article } from '../models/article.models';
+
+export class ArticleData {
+
+    getData(): Article [] {
+        let articles: Article [] = [];
+
+        for (let i = 0; i < data.length; i++) {
+            articles[i] = new Article(data[i].title, data[i].description, data[i].author, data[i].imageUrl);
+            
+        }
+
+        return articles;
+    }
+
+}
---
+++ Components/Lab/articles-app/src/app/data/seed.ts
@@ -0,0 +1,23 @@
+import { Article } from "../models/article.models";
+
+let data: Article [] = [
+{ 
+title: 'Article 3',
+description: 'LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1v',
+author: 'Author 3',
+imageUrl: 'http://i.dailymail.co.uk/i/pix/2010/04/17/article-1266852-092DE58A000005DC-112_634x411.jpg')
+},
+{
+title: 'Article 4',
+description: 'LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1v',
+author: 'Author 4',
+imageUrl: 'https://www.seoclerk.com/pics/518476-1iDFBR1489198900.png'
+},
+{
+title: 'Article 5',
+description: 'LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1LongDesc 1v',
+author: 'Author 5',
+imageUrl: 'http://www.stat.columbia.edu/~gelman/teaching/literacy/images/IV.gif'
+}]
+
+export { data };
