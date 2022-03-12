1b4b4b8f3e7d37196659eb2a875b1ebe773df86d
Petar Kobakov <pkobakov@gmail.com>
2022-03-12 17:52:27 +0200

    Create The Needed Properties

--- Components/Lab/articles-app/src/app/article/article.component.ts
+++ Components/Lab/articles-app/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../models/article.models';
 
 @Component({
@@ -7,10 +7,50 @@ import { Article } from '../models/article.models';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  private symbols: number = 250;
+  constructor() {
+    this.articleDescLen = 0;
+    this.descToShow = '';
+  }
 
-  constructor() { }
+  @Input() article!: Article;
+  @Input() articleDesc!: string;
+  descToShow: string;
+  articleDescLen: number;
+  showReadMoreBtn: boolean = true;
+  showHideBtn: boolean = false;
+  imageIsShown: boolean = false;
+  imageButtonTitle: string = 'Show Image';
 
   ngOnInit(): void {
+    
+  }
+
+  public readMore(): void {
+    this.articleDescLen += this.symbols;
+    if (this.articleDescLen >= this.articleDesc.length) {
+      this.showReadMoreBtn = false;
+      this.showHideBtn = true;
+    } else {
+      this.descToShow = this.articleDesc.substring(0, this.articleDescLen);
+    }
+  }
+
+  public toggleImage(): void {
+    if (this.imageIsShown) {
+      this.imageIsShown = false;
+      this.imageButtonTitle = 'Show Image';
+    } else {
+      this.imageIsShown = true;
+      this.imageButtonTitle = 'Hide Image';
+    }
+  }
+
+  public hideDesc(): void {
+    this.descToShow = '';
+    this.articleDescLen = 0;
+    this.showReadMoreBtn = true;
+    this.showHideBtn = false;
   }
 
 }
