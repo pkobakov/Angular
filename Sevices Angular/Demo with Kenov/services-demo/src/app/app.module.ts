de93790c21f875176103e1ed2a6525df991ada3d
Petar Kobakov <pkobakov@gmail.com>
2022-03-22 00:26:22 +0200

    Module Creating

--- Sevices Angular/Demo with Kenov/services-demo/src/app/app.module.ts
+++ Sevices Angular/Demo with Kenov/services-demo/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HomeService } from './Services/homeService';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HomeModule
   ],
-  providers: [HomeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
