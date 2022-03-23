345d6de36077074c787b7c6da0ccb810aa41e8cb
Petar Kobakov <pkobakov@gmail.com>
2022-03-23 15:58:04 +0200

    Add Bootstrap and Navigation

--- Sevices Angular/Exersice Di, Services and Observables/movie-finder/src/app/app.module.ts
+++ Sevices Angular/Exersice Di, Services and Observables/movie-finder/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
