49b9652d9e87dbcaf615b671018a67c3ad0b3e03
Petar Kobakov <pkobakov@gmail.com>
2022-03-29 01:47:19 +0300

    Template Driven Form Initial Create

---
+++ Forms/Lab/forms/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  login(formData:any) {
+    console.log(formData);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
