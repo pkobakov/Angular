94336b3a80af470bb2e3493caa9c0c59494d8039
Petar Kobakov <pkobakov@gmail.com>
2022-03-30 22:54:13 +0300

    Setup Application

---
+++ Forms/Exercise/forms-exercise/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app.routing';
+import { RegisterComponent } from './authentication/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
