716aa413fd10ec8219d1585f59cb46d983dd7da9
Petar Kobakov <pkobakov@gmail.com>
2022-04-12 15:18:56 +0300

    Exercise Authors

--- AngularWithMosh/hello-world/src/app/app.module.ts
+++ AngularWithMosh/hello-world/src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [CoursesService],
+  providers: [CoursesService,
+  AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
