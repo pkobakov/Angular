7849a0b23a05afa74682f3fad966656ec05aa0a8
Petar Kobakov <pkobakov@gmail.com>
2022-04-13 17:06:12 +0300

    Exercise Favorite Component

--- AngularWithMosh/hello-world/src/app/app.module.ts
+++ AngularWithMosh/hello-world/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SummaryPipe } from './summary.pipe';
     CoursesComponent,
     CourseComponent,
     AuthorsComponent,
-    SummaryPipe
+    SummaryPipe,
+    FavoriteComponent
   ],
   imports: [
     BrowserModule,
