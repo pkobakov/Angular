7fa321c6039a4507c140feb9a62efd08d19110d0
Petar Kobakov <pkobakov@gmail.com>
2022-04-18 21:36:36 +0300

    Adding Validation

--- AngularWithMosh/hello-world/src/app/app.module.ts
+++ AngularWithMosh/hello-world/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.compo
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     BootstrapPanelComponent,
     LikeComponent,
     InputFormatDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
