215c18c36f621c92fda480548b79e154af0cc90a
Petar Kobakov <pkobakov@gmail.com>
2022-04-20 00:51:13 +0300

    Nested Forms

--- AngularWithMosh/hello-world/src/app/signup-form/signup-form.component.ts
+++ AngularWithMosh/hello-world/src/app/signup-form/signup-form.component.ts
@@ -10,20 +10,23 @@ import { UsernameValidators } from './username.validators';
 export class SignupFormComponent {
 
   form = new FormGroup({
-     username!: new FormControl('', [
-       Validators.required,
-       Validators.minLength(3),
-       UsernameValidators.cannotContainSpace
-      ], UsernameValidators.shouldBeUnique),
-     password!: new FormControl('', Validators.required)}
-  );
+     account: new FormGroup({
+      username!: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+       ], UsernameValidators.shouldBeUnique),
+      password!: new FormControl('', Validators.required)
+     })
+
+    });
 
   get username () {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 
   get password() {
-    return this.form.get('password');
+    return this.form.get('account.password');
   }
 
   login() {
