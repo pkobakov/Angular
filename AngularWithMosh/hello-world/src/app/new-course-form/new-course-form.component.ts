5b3062d12000b90ddb407e8f70430d8e549ebd64
Petar Kobakov <pkobakov@gmail.com>
2022-04-21 02:22:08 +0300

    FormArray

--- AngularWithMosh/hello-world/src/app/new-course-form/new-course-form.component.ts
+++ AngularWithMosh/hello-world/src/app/new-course-form/new-course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -7,15 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCourseFormComponent implements OnInit {
 
-  categories = [
-    {id: 1, name: 'Development'},
-    {id: 2, name: 'Arts'},
-    {id: 3, name: 'Languages'}
-  ];
-  constructor() { }
+  // categories = [
+  //   {id: 1, name: 'Development'},
+  //   {id: 2, name: 'Arts'},
+  //   {id: 3, name: 'Languages'}
+  // ];
+  // constructor() { }
 
-  submit(value : Object){
-     console.log(value);
+  // submit(value : Object){
+  //    console.log(value);
+  // }
+
+  form = new FormGroup ({
+    topics : new FormArray([])
+  });
+ 
+  get topics() {return <FormArray>this.form.get('topics')};
+
+  addTopic(topic: HTMLInputElement) {
+      this.topics.push(new FormControl(topic.value));
+      topic.value= '';
+  }
+
+  removeTopic(topic: any) {
+   let index= this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
   }
 
   ngOnInit(): void {
