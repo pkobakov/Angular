99bf0fadb1830fb697592ddae1f89c6aa6ca0dbd
Petar Kobakov <pkobakov@gmail.com>
2022-05-03 13:18:27 +0300

    Configuring RouterModule

--- AngularWithMosh/routing/src/app/app.module.ts
+++ AngularWithMosh/routing/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +15,12 @@ import { ArchiveComponent } from './archive/archive.component';
     ArchiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path:'/archive/:year/:month', component: ArchiveComponent},
+      {path: '**', component: NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
