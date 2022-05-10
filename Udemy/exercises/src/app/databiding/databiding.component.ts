0a699558910e78dc88fa3a55ff4f6e11aca39ea6
Petar Kobakov <pkobakov@gmail.com>
2022-05-10 14:34:10 +0300

    DataBiding

---
+++ Udemy/exercises/src/app/databiding/databiding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'databiding',
+  templateUrl: './databiding.component.html',
+  styleUrls: ['./databiding.component.css']
+})
+export class DatabidingComponent implements OnInit {
+username : string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
