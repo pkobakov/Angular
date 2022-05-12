70538ba83a13e64fe3f5fd433cdf48cab6ee2988
Petar Kobakov <pkobakov@gmail.com>
2022-05-12 17:07:10 +0300

    Implement Recipe Detail

--- Udemy/course-project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ Udemy/course-project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
 recipes: Recipe [] = [
-  new Recipe ('test recipe', 'test desc' ,'http://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR-Featured-680x900.jpg')
+  new Recipe ('Test recipe',
+              'Some description here' ,
+              'http://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR-Featured-680x900.jpg'),
+  new Recipe ('Test recipe',
+              'Some description here' ,
+              'http://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR-Featured-680x900.jpg'),        
 ];
   constructor() { }
 
