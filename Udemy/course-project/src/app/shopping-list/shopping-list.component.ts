4e64fe5ce7d07a50b5f0e8a6832a793f389e45c5
Petar Kobakov <pkobakov@gmail.com>
2022-05-12 17:45:14 +0300

    Add Shopping List Edit

--- Udemy/course-project/src/app/shopping-list/shopping-list.component.ts
+++ Udemy/course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients: Ingredient[] = [
+  new Ingredient ('Apples', 5),
+  new Ingredient ('Tomatoes', 7)
+];
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
