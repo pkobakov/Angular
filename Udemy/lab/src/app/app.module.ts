b19342ccc01f20d00350c31b8351f519cfb0c3f7
Petar Kobakov <pkobakov@gmail.com>
2022-05-19 00:31:01 +0300

    Custom Directive

--- Udemy/lab/src/app/app.module.ts
+++ Udemy/lab/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BasicHighlightDirective } from 'src/basic-highlight/basic-highlight.directive';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
@@ -10,7 +11,8 @@ import { ServerComponent } from './server/server.component';
   declarations: [
     AppComponent,
     CockpitComponent,
-    ServerComponent
+    ServerComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule, 
