d340396d8a824db432f81e3f2564a2145f4e7ce3
Petar Kobakov <pkobakov@gmail.com>
2022-05-18 17:56:40 +0300

    Show Odd Numbers with ngIf and ngFor

--- Udemy/lab/src/app/app.component.ts
+++ Udemy/lab/src/app/app.component.ts
@@ -7,22 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab';
-  serverElements: any = [];
+  // serverElements: any = [];
+  numbers = [1,2,3,4,5];
+  oddNumbers = [1,3,5];
+  evenNumbers = [2,4];
+  onlyOdd:boolean = false;
 
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
+  // onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverData.serverName,
+  //     content: serverData.serverContent
+  //   });
+  // }
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
-  }
+  // onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: blueprintData.serverName,
+  //     content: blueprintData.serverContent
+  //   });
+  // }
   
 }
