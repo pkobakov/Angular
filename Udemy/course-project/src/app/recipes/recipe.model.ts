96b80ea2b691f38743563acb3d089c454d2888d7
Petar Kobakov <pkobakov@gmail.com>
2022-05-18 18:37:31 +0300

    ngIf and ngClass Recap

---
+++ Udemy/course-project/src/app/recipes/recipe.model.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imgUrl: string;
+    constructor(name: string, desc: string, imgUrl: string){
+        this.name = name,
+        this. description = desc,
+        this.imgUrl = imgUrl
+    }
+}
