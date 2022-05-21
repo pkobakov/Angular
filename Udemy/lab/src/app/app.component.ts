cc38e9268fdc1173269bfac94ec6c3370785dcad
Petar Kobakov <pkobakov@gmail.com>
2022-05-21 15:38:20 +0300

    ngSwitch

--- Udemy/lab/src/app/app.component.ts
+++ Udemy/lab/src/app/app.component.ts
@@ -7,26 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab';
-  // serverElements: any = [];
+  serverElements: any = [];
   numbers = [1,2,3,4,5];
   oddNumbers = [1,3,5];
   evenNumbers = [2,4];
   onlyOdd:boolean = false;
+  value: number = 30;
 
-  // onServerAdded(serverData: {serverName: string, serverContent: string}) {
-  //   this.serverElements.push({
-  //     type: 'server',
-  //     name: serverData.serverName,
-  //     content: serverData.serverContent
-  //   });
-  // }
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
-  // onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-  //   this.serverElements.push({
-  //     type: 'blueprint',
-  //     name: blueprintData.serverName,
-  //     content: blueprintData.serverContent
-  //   });
-  // }
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
   
 }
