cd94a740f254faaae4a4d00faca4769676ad991c
Petar Kobakov <pkobakov@gmail.com>
2022-05-26 15:27:06 +0300

    Adding Ingredient into Recipe

--- Udemy/course-project/src/app/recipes/recipe.model.ts
+++ Udemy/course-project/src/app/recipes/recipe.model.ts
@@ -1,10 +1,15 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imgUrl: string;
-    constructor(name: string, desc: string, imgUrl: string){
-        this.name = name,
-        this. description = desc,
-        this.imgUrl = imgUrl
+    public ingredients!: Ingredient [];
+    
+    constructor(name: string, desc: string, imgUrl: string, ingredients: Ingredient []){
+        this.name = name;
+        this. description = desc;
+        this.imgUrl = imgUrl;
+        this.ingredients = ingredients;
     }
 }
