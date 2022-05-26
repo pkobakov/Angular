b07fab4390c3b4fe7c8bef52c394156b800afe3a
Petar Kobakov <pkobakov@gmail.com>
2022-05-26 16:08:56 +0300

    Adding Ingredients to Recipes. Passing them from Recipes to Shopping list

--- Udemy/course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Udemy/course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipes.service';
 
 @Component({
   selector: 'recipe-detail',
@@ -9,9 +10,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
  @Input() recipe!: Recipe;
  
-  constructor() { }
+  constructor(private recipeService: RecipeService) { 
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onAddToShoppingList(){
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
 }
