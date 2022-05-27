0f42f6cbfd395e959b901cd47fb2633a52b2cf74
Petar Kobakov <pkobakov@gmail.com>
2022-05-27 15:05:17 +0300

    Parent to Child Demo

---
+++ Cross Component Communication/parent-to-child/src/app/parent/parent.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  message: string = 'Hello World!';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
