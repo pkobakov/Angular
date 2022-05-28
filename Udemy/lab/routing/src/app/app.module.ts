4a6a0f546c7f1f39810c3b0c6822d5c9abe52c72
Petar Kobakov <pkobakov@gmail.com>
2022-05-28 09:12:35 +0300

    Registering Routes

--- Udemy/lab/routing/src/app/app.module.ts
+++ Udemy/lab/routing/src/app/app.module.ts
@@ -11,6 +11,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UserComponent},
+  {path: 'servers', component: ServerComponent}
+
+
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +34,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
