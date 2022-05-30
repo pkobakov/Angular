466431754d91ed715012aeb65692b990d2a26220
Petar Kobakov <pkobakov@gmail.com>
2022-05-31 00:59:09 +0300

    Redirecting and Wildcards Routes

--- Udemy/lab/routing/src/app/app.module.ts
+++ Udemy/lab/routing/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes : Routes = [
   {path: '', component: HomeComponent},
@@ -25,7 +26,10 @@ const routes : Routes = [
       {path: ':id', component: ServerComponent},
       {path: ':id/:edit', component: EditServerComponent}
       ]
-     },
+  },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}  
+  
 ];
 
 @NgModule({
@@ -36,7 +40,8 @@ const routes : Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
