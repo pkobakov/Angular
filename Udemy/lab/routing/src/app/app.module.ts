5f0a55bb08b2834987d8eb0aaa1c186f0df3357c
Petar Kobakov <pkobakov@gmail.com>
2022-05-31 17:24:52 +0300

    Protecting Routes using CanActivate

--- Udemy/lab/routing/src/app/app.module.ts
+++ Udemy/lab/routing/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 
 
@@ -33,7 +35,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
     
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
