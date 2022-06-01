4a940082dd57c5a6f74650dd3fc0237583253c51
Petar Kobakov <pkobakov@gmail.com>
2022-06-01 16:19:37 +0300

    Controlling Navigation with CanDeactivate

--- Udemy/lab/routing/src/app/app.module.ts
+++ Udemy/lab/routing/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 
 
@@ -35,7 +36,7 @@ import { AuthGuard } from './auth-guard.service';
     AppRoutingModule
     
   ],
-  providers: [ServersService, AuthService, AuthGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
