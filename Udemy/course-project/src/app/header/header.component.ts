2b34f2cdba546d6e161567e972592bfb695fc28b
Petar Kobakov <pkobakov@gmail.com>
2022-06-07 18:36:23 +0300

    Adding Navigation and Marking Active Routes

--- Udemy/course-project/src/app/header/header.component.ts
+++ Udemy/course-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'header',
@@ -6,9 +6,6 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 
 export class HeaderComponent {
-   @Output() selectedFeature = new EventEmitter<string>();
-    onSelect(feature: string){
-        this.selectedFeature.emit(feature);
-    }
+
 
 }
