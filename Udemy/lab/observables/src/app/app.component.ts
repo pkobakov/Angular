9964d3c43ce621f3ceec84f971793d4c3175f9f0
Petar Kobakov <pkobakov@gmail.com>
2022-06-13 00:06:23 +0300

    Using Subject

--- Udemy/lab/observables/src/app/app.component.ts
+++ Udemy/lab/observables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  userActivated = false;
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    }
+      )
+  }
 }
--- Udemy/lab/observables/src/app/home/home.component.ts
+++ Udemy/lab/observables/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Observer, Subscription } from 'rxjs';
-import { map, filter} from 'rxjs/operators'
+// import { map, filter} from 'rxjs/operators'
 
 
 @Component({
@@ -10,54 +10,55 @@ import { map, filter} from 'rxjs/operators'
 })
 export class HomeComponent implements OnInit , OnDestroy{
 
-  private firstSubscription: Subscription;
+  // private firstSubscription: Subscription;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
 
     // this.firstSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // });
 
-    const customInterval: Observable<any> = Observable.create((observer: Observer<number>) => {
-      let count = 0;
-      setInterval(() => {observer.next(count);
-        if (count == 5) {
-          observer.complete();
+  //   const customInterval: Observable<any> = Observable.create((observer: Observer<number>) => {
+  //     let count = 0;
+  //     setInterval(() => {observer.next(count);
+  //       if (count == 5) {
+  //         observer.complete();
 
-        }
-        if (count > 3) {
-          observer.error(new Error('The count is greater than 3!'));
-        }
-      count++; },
-      1000);
-    });
+  //       }
+  //       if (count > 3) {
+  //         observer.error(new Error('The count is greater than 3!'));
+  //       }
+  //     count++; },
+  //     1000);
+  //   });
 
-    
-   
-
-   this.firstSubscription =  customInterval.pipe
-   (
-     filter(data => {
-     return data > 0;
-   }),
-     map((data: number) => {
-     return 'Round:' + (data + 1);
-  }))
-    .subscribe(data => {
-      console.log(data);
-    }, error => {
-     console.log(error);
-     alert(error);
-    }, 
-      () => {
-        alert('Completed!'); 
-      });
-  }
+  //  this.firstSubscription =  customInterval.pipe
+  //  (
+  //    filter(data => {
+  //    return data > 0;
+  //  }),
+  //    map((data: number) => {
+  //    return 'Round:' + (data + 1);
+  // }))
+  //   .subscribe(data => {
+  //     console.log(data);
+  //   }, error => {
+  //    console.log(error);
+  //    alert(error);
+  //   }, 
+  //     () => {
+  //       alert('Completed!'); 
+  //     });
+  // }
 
   ngOnDestroy(): void {
-    this.firstSubscription.unsubscribe();
     
   }
+
+  // ngOnDestroy(): void {
+  //   this.firstSubscription.unsubscribe();
+    
+  // }
 
 }
