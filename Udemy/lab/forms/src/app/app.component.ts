eee880b42e130559de0b2825943e53b63776e9f3
Petar Kobakov <pkobakov@gmail.com>
2022-06-14 00:11:39 +0300

    Adding Radio Buttons

--- Udemy/lab/forms/src/app/app.component.ts
+++ Udemy/lab/forms/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   defaultUsername = 'test'
   defaultQuestion = 'teacher';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
