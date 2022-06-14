421e2a114d3d6c0dbfc3cbd1660ed2adc67b83b8
Petar Kobakov <pkobakov@gmail.com>
2022-06-14 15:37:57 +0300

    Using the Form Data

--- Udemy/lab/forms/src/app/app.component.ts
+++ Udemy/lab/forms/src/app/app.component.ts
@@ -13,7 +13,15 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username:'',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
 
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
 
@@ -40,9 +48,16 @@ export class AppComponent {
   //   console.log(form);
   // }
 
-  onSubmit(){
-    console.log('Submitted!');
-    console.log(this.signupForm);
+  // 
+  
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender  = this.signupForm.value.gender;
+
   }
 
 
