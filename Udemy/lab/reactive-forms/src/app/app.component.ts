8d0e8175299b4469873c246fb16f6938c3698dbe
Petar Kobakov <pkobakov@gmail.com>
2022-06-16 00:44:13 +0300

    Reacting to Value and Status changes

--- Udemy/lab/reactive-forms/src/app/app.component.ts
+++ Udemy/lab/reactive-forms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,16 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.signupForm = new FormGroup ({
       'userData': new FormGroup ({'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-                                   'email': new FormControl(null, [Validators.required, Validators.email])}),
+                                   'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)}),
       'gender': new FormControl ('male'),
       'hobbies': new FormArray([])
     })
+
+    // this.signupForm.valueChanges.subscribe((value)=>
+    // console.log(value));
+
+    this.signupForm.statusChanges.subscribe((status)=>
+    console.log(status));
   }
 
   onSubmit() {
@@ -37,6 +44,19 @@ export class AppComponent implements OnInit{
     }
 
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any> ((resolve, reject)=> {setTimeout(() => {
+     if (control.value === 'test@test.com') {
+      resolve({'emailIsForbidden': true});
+     }  else {
+      resolve(null);
+     }  
+    }, 1500);
+  });
+   
+    return promise;
   }
 
 
