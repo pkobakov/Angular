eeccecdf41656cf6b8f39d9d0297e48e9cd55d6f
Petar Kobakov <pkobakov@gmail.com>
2022-06-15 16:46:28 +0300

    Creating Custom Validator

--- Udemy/lab/reactive-forms/src/app/app.component.ts
+++ Udemy/lab/reactive-forms/src/app/app.component.ts
@@ -9,10 +9,11 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup
+  forbiddenUserNames = ['Chris', 'Anna'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup ({
-      'userData': new FormGroup ({'username': new FormControl(null, Validators.required),
+      'userData': new FormGroup ({'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
                                    'email': new FormControl(null, [Validators.required, Validators.email])}),
       'gender': new FormControl ('male'),
       'hobbies': new FormArray([])
@@ -23,17 +24,30 @@ export class AppComponent implements OnInit{
     console.log(this.signupForm);
   }
 
-  getControls() {
-    // Add to template: 
-    // *ngFor="let hobbyControl of getControls(); let i = index"
 
-    return (<FormArray>this.signupForm.get('hobbies')).controls;
-  }
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
   }
+
+  forbiddenNames(control: FormControl): {[key: string]: boolean} {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+
+    return null;
+  }
+
+
+
+
+    // getControls() {
+  //   // Add to template: 
+  //   // *ngFor="let hobbyControl of getControls(); let i = index"
+
+  //   return (<FormArray>this.signupForm.get('hobbies')).controls;
+  // }
 
   
 }
