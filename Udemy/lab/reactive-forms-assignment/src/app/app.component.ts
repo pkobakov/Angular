4b229d2c3f974b25877792519c35b545d171b668
Petar Kobakov <pkobakov@gmail.com>
2022-06-16 17:33:47 +0300

    Adding Custom sync and async Validators

--- Udemy/lab/reactive-forms-assignment/src/app/app.component.ts
+++ Udemy/lab/reactive-forms-assignment/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'reactive-forms-assignment';
+  projectForm! : FormGroup;
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup ({
+       'projectName': new FormControl (null, [Validators.required, CustomValidators.invalidProjectName],CustomValidators.asyncInvalidProjectName),
+       'email': new FormControl (null, [Validators.required, Validators.email]),
+       'projectStatus': new FormControl ('critical')
+    });
+  }
+
+  onSaveProject(){
+    console.log(this.projectForm.value);
+  }
+  
 }
