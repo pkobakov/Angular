d4ebc8e9286b2c2e8e352cfac931094a140efdb4
Petar Kobakov <pkobakov@gmail.com>
2022-06-18 16:01:48 +0300

    Adding Ingredient Controls to a Form Array

---
+++ Udemy/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipes.service';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+  id!: number;
+  editMode = false;
+  recipeForm!: FormGroup;
+
+  constructor(private route : ActivatedRoute,
+              private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.route.params
+              .subscribe((params: Params) => {
+                this.id = +params['id'];
+                this.editMode = params['id'] != null;
+                this.initForm();
+              })
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  get controls() : any {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+}
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+
+    
+  
+    
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      console.log(recipe);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imgUrl;
+      recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for(let ingredient of recipe.ingredients){
+
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup ({
+      'name' : new FormControl(recipeName),
+      'recipeImagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
+    })
+  }
+
+}
---
+++ Udemy/course-project/src/app/recipes/recipes.service.ts
@@ -0,0 +1,45 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+
+    // selectedRecipe = new EventEmitter<Recipe>();
+    // selectedRecipe = new Subject<Recipe>();
+
+   private recipes: Recipe [] = [
+        new Recipe ('Pizza Margherita',
+                    'Some description here' ,
+                    'https://upload.wikimedia.org/wikipedia/commons/e/ee/Eq_it-na_pizza-margherita_sep2005_sml-2.png',
+                    [
+                        new Ingredient('tomato sauce', 200),
+                        new Ingredient('mozzarela', 150)    
+                    ]),
+        new Recipe ('Tasty Hamburguer',
+                    'Some description here' ,
+                    'https://www.imagensempng.com.br/wp-content/uploads/2021/08/03-19.png',
+                    [
+                        new Ingredient ('beef meat', 150),
+                        new Ingredient ('cedar', 100)
+                    ]),        
+      ];
+
+      constructor(private shoppingListService : ShoppingListService) {
+
+      }
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+
+      getRecipe(id: number) {
+          return this.recipes[id];
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngrredients(ingredients);
+      }
+}
