3f2b2badda9f89210a93f641903bfc2d74f6f41d
Petar Kobakov <pkobakov@gmail.com>
2022-06-18 16:50:34 +0300

    Adding new Ingredient Controls

--- Udemy/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Udemy/course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -70,4 +70,10 @@ export class RecipeEditComponent implements OnInit {
     })
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(new FormGroup({
+      'name': new FormControl(),
+      'amount': new FormControl()
+    }));
+  }
 }
