fb1edaa97ef5fc56426d83f3d68b2dd303a1ddca
Petar Kobakov <pkobakov@gmail.com>
2022-06-20 13:50:22 +0300

    Creating Custom Pipe

--- Udemy/lab/pipes/src/app/app.module.ts
+++ Udemy/lab/pipes/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ShortenPipe
   ],
   imports: [
     BrowserModule,
