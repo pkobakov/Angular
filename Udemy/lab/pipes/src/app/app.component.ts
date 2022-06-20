626fd9e8cc11bf68d3ced519193114480732a411
Petar Kobakov <pkobakov@gmail.com>
2022-06-20 17:09:32 +0300

    Making Data visible before print filter

--- Udemy/lab/pipes/src/app/app.component.ts
+++ Udemy/lab/pipes/src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  appStatus =  new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('stable');
+  }, 2000); 
+});
+
   servers = [
     {
       instanceType: 'medium',
@@ -32,11 +39,25 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+  
+  filteredStatus = '';
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    const server = { 
+      instanceType: 'large',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    } 
+
+    this.servers.push(server);
   }
 }
--- Udemy/lab/pipes/src/app/app.module.ts
+++ Udemy/lab/pipes/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent, ShortenPipe
+    AppComponent, ShortenPipe, FilterPipe
   ],
   imports: [
     BrowserModule,
