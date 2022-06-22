8fda9b099f3da11ab6e6527f04cbf65eda5e87bf
Petar Kobakov <pkobakov@gmail.com>
2022-06-22 14:11:40 +0300

    Using Subject for Error Handling

--- Udemy/lab/http/src/app/app.component.ts
+++ Udemy/lab/http/src/app/app.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
-  message = 'Error 401, Unauthorized';
+  message = 'Error 401, Unauthorized.Permission denied';
+  private subscription : Subscription;
 
   constructor(private http: HttpClient,
               private postsService: PostsService) {}
 
   ngOnInit() {
+    this.subscription =  this.postsService.error.subscribe(errorMessage => {
+
+      this.error = errorMessage;
+      console.log(errorMessage);
+    });
 
     this.isFetching = true;
     this.postsService.fetchPosts()
@@ -35,7 +42,8 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
 
-         this.postsService.createAndStorePost(postData.title, postData.content);
+    
+    this.postsService.createAndStorePost(postData.title, postData.content);
   
   }
 
@@ -63,5 +71,8 @@ export class AppComponent implements OnInit {
 
   }
 
+  ngOnDestroy(): void {
+  this.subscription.unsubscribe();
+}
 
 }
