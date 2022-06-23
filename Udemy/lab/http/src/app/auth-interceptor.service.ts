f820d1c0fc96cfb013578bc609dda290a973150d
Petar Kobakov <pkobakov@gmail.com>
2022-06-24 00:36:00 +0300

    Response Interceptors

--- Udemy/lab/http/src/app/auth-interceptor.service.ts
+++ Udemy/lab/http/src/app/auth-interceptor.service.ts
@@ -1,10 +1,21 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Request is running');
-        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'Petar')});
-        return next.handle(modifiedRequest);
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'Petar')
+        });
+        return next.handle(modifiedRequest).pipe(tap(event => {
+
+            console.log(event);
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data: ');
+                console.log(event.body);
+            }
+        })
+      );
     }
 }
