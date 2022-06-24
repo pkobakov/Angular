a0a9209a45fe85a7cafb4247db31e055eb72e9dd
Petar Kobakov <pkobakov@gmail.com>
2022-06-24 14:03:18 +0300

    Storing Data in the Database

--- Udemy/course-project/src/app/header/header.component.ts
+++ Udemy/course-project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
     selector: 'header',
@@ -6,6 +7,10 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
+    constructor(private dataStorageService: DataStorageService){}
 
+onSaveData(){
 
+    this.dataStorageService.storeRecipes();
+}
 }
