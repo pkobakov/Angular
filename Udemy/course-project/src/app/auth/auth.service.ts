64a721313c7a15600174cb6d1407e676a15e4525
Petar Kobakov <pkobakov@gmail.com>
2022-06-30 00:39:30 +0300

    Adding Logout

--- Udemy/course-project/src/app/auth/auth.service.ts
+++ Udemy/course-project/src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { BehaviorSubject, throwError } from "rxjs";
 import {catchError, tap} from "rxjs/operators";
 import { User } from "./user.model";
@@ -21,7 +22,8 @@ export class AuthService {
 
     user = new BehaviorSubject<User>(null);
 
-    constructor(private  http: HttpClient) {
+    constructor(private  http: HttpClient,
+                private router: Router) {
     }
 
     signup(email: string, password: string) {
@@ -60,6 +62,11 @@ export class AuthService {
                     +resData.expiresIn
                 )
             }));
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication(userId: string, email:string, token: string, expiresIn: number) {
