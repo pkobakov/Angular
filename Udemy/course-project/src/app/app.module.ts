31a8d7600027ddc69651428d8f7ed42af05d746c
Petar Kobakov <pkobakov@gmail.com>
2022-07-01 15:01:21 +0300

    Adding Modal Alert Component

--- Udemy/course-project/src/app/app.module.ts
+++ Udemy/course-project/src/app/app.module.ts
@@ -22,10 +22,12 @@ import { AuthService } from './auth/auth.service';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthGuard } from './auth/auth.guard';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlertComponent,
     AuthComponent,
     HeaderComponent,
     RecipesComponent,
