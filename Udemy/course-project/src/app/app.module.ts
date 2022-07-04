fc35c060632f68da81b35ccea161cca2f9eb8c30
Petar Kobakov <pkobakov@gmail.com>
2022-07-04 18:07:24 +0300

    Creating Shopping List Module

--- Udemy/course-project/src/app/app.module.ts
+++ Udemy/course-project/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +19,7 @@ import { AuthGuard } from './auth/auth.guard';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceHolderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
@@ -29,8 +28,6 @@ import { RecipesModule } from './recipes/recipes.module';
     AuthComponent,
     HeaderComponent,
     PlaceHolderDirective,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     LoadingSpinnerComponent,
     DropdownDirective,
     
@@ -41,7 +38,8 @@ import { RecipesModule } from './recipes/recipes.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [ShoppingListService,
               RecipeService,
