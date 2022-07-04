4f70f6108379eb78b60b7c2f460d551b4715a65e
Petar Kobakov <pkobakov@gmail.com>
2022-07-04 17:54:26 +0300

    Component Declarations

--- Udemy/course-project/src/app/app-routing.module.ts
+++ Udemy/course-project/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
