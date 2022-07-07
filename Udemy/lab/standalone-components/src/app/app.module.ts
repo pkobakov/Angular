3acd049e5c745428b7400b6366d6396d475827e0
Petar Kobakov <pkobakov@gmail.com>
2022-07-07 18:19:34 +0300

    Building the First Standalone Component

--- Udemy/lab/standalone-components/src/app/app.module.ts
+++ Udemy/lab/standalone-components/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
-  imports: [BrowserModule, SharedModule],
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, SharedModule, DetailsComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Udemy/lab/standalone-components/src/app/welcome/details/details.component.ts
+++ Udemy/lab/standalone-components/src/app/welcome/details/details.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { HighlightDirective } from 'src/app/shared/highlight.directive';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
+  standalone: true,
+  imports: [SharedModule],
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
