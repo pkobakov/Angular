e5eb5662a23365553e3a0983ffcee4c958c88de6
Petar Kobakov <pkobakov@gmail.com>
2022-07-18 17:20:09 +0300

    Working with NgRx. Creating the initial state

---
+++ Udemy/course-project/src/app/core.module.ts
@@ -0,0 +1,17 @@
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { AuthInterceptorService } from "./auth/auth-interceptor.service";
+import { AuthService } from "./auth/auth.service";
+import { RecipeService } from "./recipes/recipe.service";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
+
+@NgModule({
+    providers: [
+                 ShoppingListService,
+                 RecipeService,
+                 AuthService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+    ]
+})
+export class CoreModule {
+
+}
