2690c63a7d86375be36cca16acbf07e76d364e39
Petar Kobakov <pkobakov@gmail.com>
2022-07-19 01:18:24 +0300

    Fixing the tsconfig.json file by setting module prop to ESNext

--- Udemy/course-project/src/app/app.module.ts
+++ Udemy/course-project/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { StoreModule } from '@ngrx/store';
 import { shoppingListReducer } from './shopping-list/store/shopping.reducer';
 
@@ -24,9 +21,6 @@ import { shoppingListReducer } from './shopping-list/store/shopping.reducer';
     AppRoutingModule,
     SharedModule,
     CoreModule,
-    ShoppingListModule,
-    AuthModule,
-    RecipesModule,
     StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
               
