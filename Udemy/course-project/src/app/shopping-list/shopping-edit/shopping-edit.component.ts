8e70ce3b9202769c575dfe51e300719251ebde04
Petar Kobakov <pkobakov@gmail.com>
2022-07-21 15:59:56 +0300

    Dispatching Actions

--- Udemy/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Udemy/course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -5,10 +5,12 @@ import {
   ViewChild
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -22,7 +24,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[] }}>) { }
 
   ngOnInit() {
     this.subscription = this.slService.startedEditing
@@ -45,7 +48,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     if (this.editMode) {
       this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     } else {
-      this.slService.addIngredient(newIngredient);
+      // this.slService.addIngredient(newIngredient);
+      this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
     this.editMode = false;
     form.reset();
