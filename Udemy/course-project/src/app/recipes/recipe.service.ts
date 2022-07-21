3af26fe506be00e51343a02e50dbb9512913e76e
Petar Kobakov <pkobakov@gmail.com>
2022-07-21 16:57:03 +0300

    Dispatching Multiple Actions

--- Udemy/course-project/src/app/recipes/recipe.service.ts
+++ Udemy/course-project/src/app/recipes/recipe.service.ts
@@ -4,6 +4,8 @@ import { Subject } from 'rxjs';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 @Injectable()
 export class RecipeService {
@@ -25,7 +27,8 @@ export class RecipeService {
   // ];
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService,
+              private store: Store<{shoppingList: {ingredients: Ingredient[] }}>) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -41,7 +44,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
+    // this.slService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   addRecipe(recipe: Recipe) {
--- Udemy/course-project/src/app/shopping-list/store/shopping-list.reducer.ts
+++ Udemy/course-project/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -7,7 +7,7 @@ const initialState = {
 
 export function shoppingListReducer(
   state = initialState,
-  action: ShoppingListActions.AddIngredient
+  action:  ShoppingListActions.ShoppingListActions
 ) {
   switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
@@ -15,6 +15,12 @@ export function shoppingListReducer(
         ...state,
         ingredients: [...state.ingredients, action.payload]
       };
+
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state, ingredients: [...state.ingredients, ...action.payload]
+      };  
+
     default:
       return state;
   }
