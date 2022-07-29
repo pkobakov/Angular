a5b6875106700cf379058066b447c4e67c011249
Petar Kobakov <pkobakov@gmail.com>
2022-07-30 00:46:20 +0300

    Finishing the Login Effect

--- Udemy/course-project/src/app/auth/auth.component.ts
+++ Udemy/course-project/src/app/auth/auth.component.ts
@@ -38,9 +38,11 @@ export class AuthComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.store.select('auth').subscribe(authState => {
-
       this.isLoading = authState.loading;
       this.error = authState.authError;
+      if(this.error){
+        this.showErrorAlert(this.error);
+      }
 
     });
   }
