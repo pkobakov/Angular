ff894ea9b02cfa9609372da0a16875475182f0c3
Petar Kobakov <pkobakov@gmail.com>
2022-08-01 17:29:58 +0300

    Using Store Devtools

--- Udemy/course-project/src/app/auth/auth.module.ts
+++ Udemy/course-project/src/app/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import { AuthComponent } from './auth.component';
 import { SharedModule } from '../shared/shared.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AuthComponent],
@@ -12,7 +14,8 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: AuthComponent }]),
-    SharedModule
+    SharedModule,
+    StoreDevtoolsModule.instrument({logOnly: environment.production})
   ]
 })
 export class AuthModule {}
